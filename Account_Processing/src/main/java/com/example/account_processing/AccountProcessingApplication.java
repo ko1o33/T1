ad70948d0a306b11b78882c20e1d41b983ba35ab
vbif1 <vbif12345678900@mail.ru>
2025-09-25 18:28:04 +0300

    complete

---
+++ Account_Processing/src/main/java/com/example/account_processing/AccountProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.account_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountProcessingApplication.class, args);
+    }
+
+}
---
+++ Credit_Processing/src/test/java/com/example/credit_processing/CreditProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.credit_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
